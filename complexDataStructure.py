b7eadd4062ae8e753f7c6ac328740ce850039f0f
Justin Murphy <98184998+Justin-Murphy-2003@users.noreply.github.com>
2022-04-07 00:56:09 -0400

    Update complexDataStructure.py

--- complexDataStructure.py
+++ complexDataStructure.py
@@ -35,15 +35,15 @@ def main():
     add_pizza_toppings(dict, extra_pizza_toppings)
     list.sort(dict['pizza_toppings'])
     print(dict)
-    #sentences(dict)
+    sentences(dict)
     
 def add_pizza_toppings(dict, extra_pizza_toppings):
     for p in extra_pizza_toppings:
         dict['pizza_toppings'].append(p)
 
 def sentences(dict):
-    print("Hi, my name is" "and my student number is" ),
-    print("My ideal pizza has"),
-    print("I like to watch"),
-    print("Some of my favourites are")
+    print("Hi Jeremy, my name is", dict('name'), "and my student number is"),
+    print("My ideal pizza has", dict('pizza_toppings')),
+    print("I like to watch", "movies"),
+    print("Some of my favourites are", "idk")
 main()
