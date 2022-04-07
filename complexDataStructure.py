f91b0e2a8fab47dcef02113cca28a0789b1d6e16
Justin Murphy <98184998+Justin-Murphy-2003@users.noreply.github.com>
2022-04-07 19:19:17 -0400

    Update complexDataStructure.py
    I think I did it. That was great.

--- complexDataStructure.py
+++ complexDataStructure.py
@@ -21,12 +21,11 @@ def main():
         ]
     }
 
-    another_movie = [
-        {
+    another_movie = {
         'name' : 'The Matrix', 
         'genre' : 'action'
-        }
-    ]
+    }
+    
     extra_pizza_toppings = {
         'bacon',
         'sausage'
@@ -42,8 +41,30 @@ def add_pizza_toppings(dict, extra_pizza_toppings):
         dict['pizza_toppings'].append(p)
 
 def sentences(dict):
-    print("Hi Jeremy, my name is", dict('name'), "and my student number is"),
-    print("My ideal pizza has", dict('pizza_toppings')),
-    print("I like to watch", "movies"),
-    print("Some of my favourites are", "movies")
+    print("Hi Jeremy, my name is", dict['name'], "and my student number is", dict['student_id'])
+    toppings = ''
+    for p in dict['pizza_toppings']:
+        toppings += p + ' '
+    print("My ideal pizza has", toppings)
+    genres = ''
+    for m,g in enumerate(dict['movies']):
+        genres += g['genre']
+        if m == len(dict['movies']) -2:
+            genres += ', & '
+        elif m == len(dict['movies']) -1:
+            genres += ' '
+        else:
+            genres += ', '
+    print("I like to watch", genres + "movies")
+
+    movie_names = ''
+    for m,g in enumerate(dict['movies']):
+        movie_names += g['name']
+        if m == len(dict['movies']) -2:
+            movie_names += ', & '
+        elif m == len(dict['movies']) -1:
+            movie_names += ' '
+        else:
+            movie_names += ', '
+    print("Some of my favourites are " + movie_names)
 main()
